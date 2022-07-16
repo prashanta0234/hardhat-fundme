1efc5df5d4b9ff3f4e732e45e23ff4a0cef8e8b3
prashanta0234 <prashanta0234@gmail.com>
2022-07-17 01:56:15 +0600

    update contract

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -8,6 +8,7 @@ error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
+    AggregatorV3Interface public s_priceFeed;
 
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
@@ -16,21 +17,22 @@ contract FundMe {
     address public /* immutable */ i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
-    constructor() {
+    constructor(address priceFeedAdress) {
         i_owner = msg.sender;
+        s_priceFeed=AggregatorV3Interface(priceFeedAdress) ;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
     
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.version();
-    }
+    // function getVersion() public view returns (uint256){
+    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    //     return priceFeed.version();
+    // }
     
     modifier onlyOwner {
         // require(msg.sender == owner);
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,5 @@ require('hardhat-deploy');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork:'hardhat'
 };
